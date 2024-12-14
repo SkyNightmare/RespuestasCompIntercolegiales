dde1845e39d1d44292e6cd36fc89b9c1dc6d5cdd
Angel <87504800+SkyNightmare@users.noreply.github.com>
2024-12-13 21:10:19 -0400

    Update FechasDelFuturo.java
    arreglo de codigo

--- FechasDelFuturo/FechasDelFuturo.java
+++ FechasDelFuturo/FechasDelFuturo.java
@@ -26,31 +26,40 @@ public class App {
         // dia
         String dia = fecha.substring(3, 5);
         int dia_n = Integer.parseInt(dia);
+        boolean hayError = false;
 
         if (year_n < 1 || year_n > 2100)
             System.out.println("El año es incorrecto, trate de nuevo.");
+        hayError = true;
 
         if (mes_n > 12) {
             System.out.println("El mes es incorrecto, trade de nuevo");
+            hayError = true;
+
         }
 
         if (mes_n == 04 || mes_n == 6 || mes_n == 9 || mes_n == 11 && dia_n == 31) {
             System.out.println("El día es incorrecto para el mes indicado, trate de nuevo.");
+            hayError = true;
+
         }
 
         if (mes_n == 02 && dia_n > 28 && (year_n & 3) != 0 && ((year_n % 25) != 0 || (year_n & 15) != 0)) {
             System.out.println("El día es incorrecto para el a~o indicado, trate de nuevo.");
+            hayError = true;
+
         }
 
-        if (mes_n == 02 && dia_n == 29 && (year_n & 3) == 0 && ((year_n % 25) == 0 || (year_n & 15) == 0)) {
-            System.out.println("A~o bisiesto puede ser 29");
-        }
         if (mes_n == 02 && dia_n > 29) {
             System.out.println("El dia es incorrecto para este mes");
+            hayError = true;
+
         }
 
-        LocalDate fechaFutura = date.plusDays(in_num);
-        System.out.println("La fecha " + in_num + " días en el futuro es " + fechaFutura.format(formatter));
-
+        if (!hayError) {
+            LocalDate fechaFutura = date.plusDays(in_num);
+            System.out.println("La fecha " + in_num + " días en el futuro es " + fechaFutura.format(formatter));
+        }
     }
+
 }
